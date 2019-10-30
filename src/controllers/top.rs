257c922bfb51dadb6c012c0dca3e19d254855cc4
sinsoku <sinsoku.listy@gmail.com>
2019-10-31 01:59:53 +0900

    Return html using templates
    refs:
    
    * https://github.com/actix/examples/blob/9e28d2ec8b87af990584307e7519257a2f1063ee/template_handlebars/src/main.rs
    * https://www.reddit.com/r/rust/comments/9n3dji/biblers_a_web_application_written_in_rust_using/

--- src/controllers/top.rs
+++ src/controllers/top.rs
@@ -1,5 +1,11 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::{web, HttpResponse, Responder};
+use handlebars::Handlebars;
 
-pub fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+pub fn index(hb: web::Data<Handlebars>) -> impl Responder {
+    let data = json!({
+        "caption": "Hello world!"
+    });
+    let res = hb.render("top/index", &data).unwrap();
+
+    HttpResponse::Ok().body(res)
 }
--- tests/main.rs
+++ tests/main.rs
@@ -4,19 +4,34 @@ mod tests {
 
     use actix_web::dev::Service;
     use actix_web::http::StatusCode;
-    use actix_web::{test, App};
+    use actix_web::{test, web, App};
     use bytes::Bytes;
+    use handlebars::Handlebars;
     use rust_web::routes;
+    use std::str;
 
     #[test]
     fn get_root() {
-        let mut app =
-            test::init_service(App::new().configure(routes::top).configure(routes::posts));
+        let mut handlebars = Handlebars::new();
+        handlebars
+            .register_templates_directory(".hbs", "./src/templates")
+            .unwrap();
+        let handlebars_ref = web::Data::new(handlebars);
+
+        let mut app = test::init_service(
+            App::new()
+                .register_data(handlebars_ref.clone())
+                .configure(routes::top)
+                .configure(routes::posts),
+        );
         let req = test::TestRequest::get().uri("/").to_request();
         let resp = test::block_on(app.call(req)).unwrap();
 
         assert_eq!(resp.status(), StatusCode::OK);
-        assert_eq!(test::read_body(resp), Bytes::from_static(b"Hello world!"));
+        let html = str::from_utf8(&test::read_body(resp))
+            .expect("Could not convert response to UTF8")
+            .to_string();
+        assert!(html.contains("<h1>Hello world!</h1>"));
     }
 
     #[test]
